a6518668d3b26a5dbbfca1ac0edb7c5510b6edd7
Matthew Hipp <74476804+hippmatthew@users.noreply.github.com>
2023-08-23 20:25:56 -0400

    Device Implementation
    * Implemented logical device creation
    * Implemented graphics and compute queues
    * Minor readme changes

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 **P**hysics **E**ngine _for_ **C**omputation _and_ **S**imulation, _By Matthew Hipp_
 
 PECS is a physics engine designed for gpu accelerated physics computation and simulation using VulkanSDK.
-THe idea is to have a separate compuational and graphical components which accesss the GPU's compute or graphics queue family respectively. The engine itself will contain the window, vulkan instance, physical device, and logical deice as well as access to the graphical and computational components if one wishes to use them.
+The idea is to have a separate compuational and graphical components which accesss the GPU's compute or graphics queue family respectively. The engine itself will contain the window, vulkan instance, physical device, and logical device as well as access to the graphical and computational components if one wishes to use them.
 
 Current Supported Vulkan Version: **_1.3.261.0_**
 
--- src/device.cpp
+++ src/device.cpp
@@ -2,7 +2,7 @@
  *  PECS - device.cpp
  *  Author:     Matthew Hipp
  *  Created:    7/21/23
- *  Updated:    7/22/23
+ *  Updated:    7/23/23
  */
 
 #include "include/device.hpp"
@@ -12,8 +12,17 @@ namespace pecs
 
 Device::Device(const vk::Instance& instance, const DebugManager * dm) : debugManager(dm)
 {
-    if (debugManager->isEnabled()) debugManager->message("picking physical device...");
+    if (debugManager->isEnabled()) debugManager->message("\tpicking physical device...");
     choosePhysicalDevice(instance);
+
+    if (debugManager->isEnabled()) debugManager->message("\tcreating logical device...");
+    createLogicalDevice();
+}
+
+Device::~Device()
+{
+    if (debugManager->isEnabled()) debugManager->message("\tdestroying logical device...");
+    logicalDevice.destroy();
 }
 
 vk::PhysicalDeviceProperties Device::getPhysicalDeviceProperties() const
@@ -32,10 +41,10 @@ void Device::choosePhysicalDevice(const vk::Instance& instance)
     switch(result)
     {
         case vk::Result::eSuccess:
-            if (debugManager->isEnabled()) debugManager->message("\tavailable devices:");
+            if (debugManager->isEnabled()) debugManager->message("\t\tavailable devices:");
             break;
         case vk::Result::eIncomplete:
-            if (debugManager->isEnabled()) debugManager->message("\twarning: not all devices accounted for\n\tavailable devices:");
+            if (debugManager->isEnabled()) debugManager->message("\t\twarning: not all devices accounted for\n\tavailable devices:");
             break;
         default:
             debugManager->message(result);
@@ -49,7 +58,7 @@ void Device::choosePhysicalDevice(const vk::Instance& instance)
         for (const auto& device : suitableDevices)
         {
             auto properties = device.getProperties();
-            debugManager->message("\t\t" + static_cast<std::string>(properties.deviceName) + " : " + vkPhysicalDeviceTypeToString(properties.deviceType));
+            debugManager->message("\t\t\t" + static_cast<std::string>(properties.deviceName) + " : " + vkPhysicalDeviceTypeToString(properties.deviceType));
         }
     }
     
@@ -58,8 +67,52 @@ void Device::choosePhysicalDevice(const vk::Instance& instance)
     if (debugManager->isEnabled())
     {
         auto properties = physicalDevice.getProperties();
-        debugManager->message("\tchosen physical device: " + static_cast<std::string>(properties.deviceName));
+        debugManager->message("\t\tchosen physical device: " + static_cast<std::string>(properties.deviceName));
     }
+}
+
+void Device::createLogicalDevice()
+{
+    auto queueFamiles = physicalDevice.getQueueFamilyProperties();
+    QueueFamilyIndices indices = findPhysicalDeviceQueueFamilyIndicies(physicalDevice, queueFamiles);
+
+    float graphicsQueuePriority = 1.0f;
+    vk::DeviceQueueCreateInfo graphicsQueueCreateInfo{ .queueFamilyIndex    = indices.graphics.value(),
+                                                       .queueCount          = 1,
+                                                       .pQueuePriorities    = &graphicsQueuePriority };
+
+    float computeQueuePriority = 1.0f;
+    vk::DeviceQueueCreateInfo computeQueueCreateInfo{ .queueFamilyIndex = indices.compute.value(),
+                                                      .queueCount       = 1,
+                                                      .pQueuePriorities = &computeQueuePriority };
+
+    std::vector<vk::DeviceQueueCreateInfo> queueCreateInfos = { graphicsQueueCreateInfo, computeQueueCreateInfo };
+
+    vk::PhysicalDeviceFeatures physicalDeviceFeatures{};
+
+    vk::DeviceCreateInfo deviceCreateInfo{ .queueCreateInfoCount     = 2,
+                                           .pQueueCreateInfos        = queueCreateInfos.data(),
+                                           .enabledLayerCount        = 0,
+                                           .enabledExtensionCount    = 0,
+                                           .pEnabledFeatures         = &physicalDeviceFeatures };
+
+    vk::Result result = physicalDevice.createDevice(&deviceCreateInfo, nullptr, &logicalDevice);
+
+    switch (result)
+    {
+        case vk::Result::eSuccess:
+            if (debugManager->isEnabled()) debugManager->message("\t\tlogical device created");
+            break;
+        default:
+            debugManager->message(result);
+    }
+
+    
+    logicalDevice.getQueue(indices.graphics.value(), 0, &graphicsQueue);
+    if (debugManager->isEnabled()) debugManager->message("\t\testablished graphics queue");
+
+    logicalDevice.getQueue(indices.compute.value(), 0, &computeQueue);
+    if (debugManager->isEnabled()) debugManager->message("\t\testablished compute queue");
 }
 
 std::vector<vk::PhysicalDevice> Device::getSuitablePhysicalDevices(const std::vector<vk::PhysicalDevice>& devices) const

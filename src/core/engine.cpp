30027116f676e669d29b71500425b786da2f7cef
Matthew Hipp <74476804+hippmatthew@users.noreply.github.com>
2024-06-21 19:01:43 -0400

    VECS 0.0.7.1
    Bug Fixes:
    - Forgot to update the engine version

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -1,7 +1,7 @@
 #include "src/core/include/engine.hpp"
 
 #define VK_VALIDATION_LAYER_NAME "VK_LAYER_KHRONOS_validation"
-#define VECS_ENGINE_VERSION       VK_MAKE_API_VERSION(0, 0, 6, 5)
+#define VECS_ENGINE_VERSION       VK_MAKE_API_VERSION(0, 0, 7, 0)
 
 namespace vecs
 {
